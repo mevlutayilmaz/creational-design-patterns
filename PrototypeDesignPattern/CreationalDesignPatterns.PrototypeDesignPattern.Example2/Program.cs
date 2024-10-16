6492f9540cf34ac044ed5d05074cb4ee92dd5e3e
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-10-17 00:53:45 +0300

    prototype design pattern example

---
+++ PrototypeDesignPattern/CreationalDesignPatterns.PrototypeDesignPattern.Example2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Person person1 = new("Ahmet", "Yazıcı", Department.A, 100, 10);
+
+Person? person2 = person1.Clone() as Person;
+person2.Name = "Mehmet";
+person2.Department = Department.B;
+
+enum Department
+{
+    A, B, C
+}
+
+#region Concrete Prototype
+class Person : ICloneable
+{
+    public Person(string name, string surname, Department department, int salary, int premium)
+    {
+        Name = name;
+        Surname = surname;
+        Department = department;
+        Salary = salary;
+        Premium = premium;
+        Console.WriteLine($"{nameof(Person)} is created.");
+    }
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public Department Department { get; set; }
+    public int Salary { get; set; }
+    public int Premium { get; set; }
+
+    public object Clone()
+    {
+        return base.MemberwiseClone();
+    }
+}
+#endregion
