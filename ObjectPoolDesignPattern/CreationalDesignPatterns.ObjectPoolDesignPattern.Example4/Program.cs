999dd89a987b50e8b3bb25705e63afc3d4d24bd1
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-10-18 00:40:44 +0300

    object pool design pattern example

---
+++ ObjectPoolDesignPattern/CreationalDesignPatterns.ObjectPoolDesignPattern.Example4/Program.cs
@@ -0,0 +1,31 @@
+﻿//Microsoft.Extensions.ObjectPool kütüphanesi ile Object Pool Design Pattern
+using Microsoft.Extensions.ObjectPool;
+
+DefaultObjectPoolProvider provider = new();
+ObjectPool<X> pool = provider.Create(new DefaultPooledObjectPolicy<X>());
+X x1 = pool.Get();
+x1.Count++;
+x1.Write();
+pool.Return(x1);
+
+X x2 = pool.Get();
+x2.Count++;
+x2.Write();
+pool.Return(x2);
+
+X x3 = pool.Get();
+x3.Count++;
+x3.Write();
+pool.Return(x3);
+
+
+class X
+{
+    public int Count { get; set; }
+    public void Write()
+        => Console.WriteLine(Count);
+    public X()
+        => Console.WriteLine("X is created.");
+    ~X()
+        => Console.WriteLine("X is destroyed.");
+}
