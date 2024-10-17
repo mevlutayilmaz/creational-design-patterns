df3cd6aaa710a20f0ab1175f58bbea0cb1590b2f
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-10-18 00:51:16 +0300

    object pool design pattern example

---
+++ ObjectPoolDesignPattern/CreationalDesignPatterns.ObjectPoolDesignPattern.Example5/Classes/X.cs
@@ -0,0 +1,13 @@
+﻿namespace CreationalDesignPatterns.ObjectPoolDesignPattern.Example5.Classes
+{
+    public class X
+    {
+        public int Count { get; set; }
+        public void Write()
+            => Console.WriteLine(Count);
+        public X()
+            => Console.WriteLine("X is created.");
+        ~X()
+            => Console.WriteLine("X is destroyed.");
+    }
+}
---
+++ ObjectPoolDesignPattern/CreationalDesignPatterns.ObjectPoolDesignPattern.Example5/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using CreationalDesignPatterns.ObjectPoolDesignPattern.Example5.Classes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ObjectPool;
+
+//Asp.NET Core mimarisinde Object Pool Design Pattern 
+namespace CreationalDesignPatterns.ObjectPoolDesignPattern.Example5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly ObjectPool<X> _pool;
+
+        public ValuesController(ObjectPool<X> pool)
+        {
+            _pool = pool;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Get1()
+        {
+            X x = _pool.Get();
+            x.Count++;
+            _pool.Return(x);
+            return Ok(x.Count);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Get2()
+        {
+            X x = _pool.Get();
+            x.Count++;
+            _pool.Return(x);
+            return Ok(x.Count);
+        }
+    }
+}
