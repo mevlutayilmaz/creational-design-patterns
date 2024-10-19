be756ec852951fae978fbe6c086ea15993548054
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-10-19 15:21:48 +0300

    builder design pattern example

---
+++ BuilderDesignPattern/CreationalDesignPatterns.BuilderDesignPattern.Example2/Program.cs
@@ -0,0 +1,136 @@
+﻿//Abstract class ile Builder Design Pattern
+
+ArabaDirector director = new();
+Araba opel = director.Build(new OpelBuilder());
+Araba mercedes = director.Build(new MercedesBuilder());
+Araba toyota = director.Build(new ToyotaBuilder());
+
+opel.ToString();
+mercedes.ToString();
+toyota.ToString();
+
+#region Product
+class Araba
+{
+    public string Marka { get; set; }
+    public string Model { get; set; }
+    public double KM { get; set; }
+    public bool Vites { get; set; }
+    public override string ToString()
+    {
+        Console.WriteLine($"{Marka} marka araba {Model} modelinde {KM} kilometrede {Vites} vites olarak üretilmiştir.");
+        return base.ToString();
+    }
+}
+#endregion
+
+#region Abstract Builder
+abstract class ArabaBuilder
+{
+    protected Araba araba;
+    public Araba Araba { get => araba; }
+    public ArabaBuilder()
+        => araba = new();
+    public abstract ArabaBuilder SetMarka();
+    public abstract ArabaBuilder SetModel();
+    public abstract ArabaBuilder SetKM();
+    public abstract ArabaBuilder SetVites();
+}
+#endregion
+
+#region Concrete Builders
+class OpelBuilder : ArabaBuilder
+{
+    public override ArabaBuilder SetKM()
+    {
+        araba.KM = 0;
+        return this;
+    }
+
+    public override ArabaBuilder SetMarka()
+    {
+        araba.Marka = "Opel";
+        return this;
+    }
+
+    public override ArabaBuilder SetModel()
+    {
+        araba.Model = "Astra";
+        return this;
+    }
+
+    public override ArabaBuilder SetVites()
+    {
+        araba.Vites = true;
+        return this;
+    }
+}
+
+class MercedesBuilder : ArabaBuilder
+{
+    public override ArabaBuilder SetKM()
+    {
+        araba.KM = 0;
+        return this;
+    }
+
+    public override ArabaBuilder SetMarka()
+    {
+        araba.Marka = "Mercedes";
+        return this;
+    }
+
+    public override ArabaBuilder SetModel()
+    {
+        araba.Model = "Maybach";
+        return this;
+    }
+
+    public override ArabaBuilder SetVites()
+    {
+        araba.Vites = true;
+        return this;
+    }
+}
+
+class ToyotaBuilder : ArabaBuilder
+{
+    public override ArabaBuilder SetKM()
+    {
+        araba.KM = 0;
+        return this;
+    }
+
+    public override ArabaBuilder SetMarka()
+    {
+        araba.Marka = "Toyota";
+        return this;
+    }
+
+    public override ArabaBuilder SetModel()
+    {
+        araba.Model = "Corolla";
+        return this;
+    }
+
+    public override ArabaBuilder SetVites()
+    {
+        araba.Vites = false;
+        return this;
+    }
+}
+#endregion
+
+#region Director
+class ArabaDirector
+{
+    public Araba Build(ArabaBuilder arabaBuilder)
+    {
+        arabaBuilder.SetMarka()
+            .SetModel()
+            .SetKM()
+            .SetVites();
+        return arabaBuilder.Araba;
+    }
+}
+#endregion
